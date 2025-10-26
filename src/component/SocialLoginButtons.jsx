c5bcecb9b7e87ccb26a38dc5eec4074f6ea9ea61
Fariba Shahsavan <f.shahsavanpour@gmail.com>
2025-10-26 16:39:49 +0330

    dir

--- src/component/SocialLoginButtons.jsx
+++ src/component/SocialLoginButtons.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../index.css";
+import "../index.css";
 import { Chip, Stack } from "@mui/material";
 import AppleIcon from '@mui/icons-material/Apple';
 import GoogleIcon from '@mui/icons-material/Google';
